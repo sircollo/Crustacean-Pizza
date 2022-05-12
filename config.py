a41e2ad1ea8f1b68bd9dc5fc28fa931f402d9fc1
Collins Sirwani <sircollo71@gmail.com>
2022-05-12 11:40:59 +0300

    Added database configurations

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-  pass
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class TestConfig(Config):
   pass
@@ -10,6 +11,10 @@ class ProdConfig(Config):
   pass
 
 class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pizzadb'
+  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adeh:adeh123!@localhost/pizzadb'
+  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:dclxvi@localhost/pizzadb'
+  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://teresiah:1234@localhost/pizzadb'
   DEBUG = True
   
 config_options = {
