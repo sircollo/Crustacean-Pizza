884dea64c76bc2b1ff9841958feb9c448fa022e6
Collins Sirwani <sircollo71@gmail.com>
2022-05-13 09:52:13 +0300

    Added more files

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,8 +3,19 @@ from flask_login import login_user
 from . import auth
 from ..models import User,Pizza,Topping
 from ..import db
+from .forms import LoginForm
 
 
 @auth.route('/admin')
 def login():
-    pass
+  login_form = LoginForm()
+  if login_form.validate_on_submit():
+    user = User.query.filter_by(email = login_form.email.data).first()
+    if user is not None and user.verify_password(login_form.password.data):
+      login_user(user,login_form.remember.data)
+      return redirect(request.args.get('next') or url_for('main.index'))
+    
+    flash('Invalid username or password')
+      
+  return render_template('auth/login.html', login_form = login_form)
+
--- config.py
+++ config.py
@@ -15,9 +15,9 @@ class ProdConfig(Config):
   pass
 
 class DevConfig(Config):
-  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pizzadb'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pizzadb'
   # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adeh:adeh123!@localhost/pizzadb'
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:dclxvi@localhost/pizzadb'
+  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:dclxvi@localhost/pizzadb'
   # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://teresiah:1234@localhost/pizzadb'
   DEBUG = True
   
